8611718c2965218fc364c3e2e6360b66c98daadc
avarelop <alvaro.valera@samara.energy>
2024-03-03 19:00:29 +0100

    feat: update code to match newer requeriments

--- devtalles/node_js/1_first_exercise/app.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const data = fs.readFileSync('README.md', 'utf8');
-const newData = data.replace(/React/ig, 'Angular');
-
-fs.writeFileSync('updated_readme.md', newData);
